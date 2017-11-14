a7481fae735f4dc414182f6715cf2bd692af173a
Katrina Chan <katrina.hyc@gmail.com>
2017-11-14 17:42:45 +0800

    Add TF-IDF case

--- gvsm.py
+++ gvsm.py
@@ -22,16 +22,22 @@ class CorpusVector(object):
             else:
                 self.terms.append(token.lower())
 
-    def get_weights(self, ref_terms=[]):
-        # Implement TF-IDF
-        return np.array([self.terms.count(term) for term in ref_terms])
+    def get_weights(self, ref_terms, weight_type='', ref_term_counts=[], N=0):
+        fs = [self.terms.count(term) for term in ref_terms]
+        if weight_type == 'tfidf' and len(ref_terms) == len(ref_term_counts) and N > 0:
+            tfs = np.array([1 + math.log2(f) if f > 0 else 0 for f in fs])
+            idf = np.array([math.log2(N/n) for n in ref_term_counts])
+            return tfs / idf
+        else:
+            return np.array(fs)
 
 class IndexTermVector(object):
     DBdoc_vectors = None
 
-    def __init__(self, i, index_terms=[]):
+    def __init__(self, i, index_terms=[], index_term_counts=[], weight_type=''):
         self.i = i
         self.index_terms = index_terms
+        self.index_term_counts = index_term_counts
 
         t = len(index_terms)
         self.minterms = np.array([0, 1])[np.rollaxis(
@@ -47,9 +53,11 @@ class IndexTermVector(object):
         np.fill_diagonal(self.minterm_vectors, 1)
 
     def get_vector(self):
+        """Return the index term vector of the term given by `i`."""
         cirs = [self.calculate_cir(m) for m in self.minterms]
+
         # Calculate the numerator
-        numer = np.zeros((len(self.minterms),), int)
+        numer = np.zeros((len(self.minterms),))
         for r in range(len(self.minterms)):
             add = self.get_on(self.minterms[r]) * cirs[r] * self.minterm_vectors[r]
             numer = np.add(numer, add)
